00c800d1891f73bbeb758d45d5011cee73fa694d
Dan Wagner <dkwagner@mtu.edu>
2019-12-29 19:02:42 -0600

    Adding tests because all honorable side projects are tdd'd

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,22 +4,22 @@ import (
 	"polite-script/token"
 )
 
-var reservedPhrases map[string]string
+// var reservedPhrases map[string]string
 
-func init() {
-	reservedPhrases[token.KEYPHRASE_START] = "Please"
-	reservedPhrases[token.KEYPHRASE_END] = "Thanks"
-	reservedPhrases[token.KEYPHRASE_IF] = "check if"
-	reservedPhrases[token.KEYPHRASE_DECLARE] = "set"
-	reservedPhrases[token.KEYPHRASE_LOOP] = "loop while"
-	reservedPhrases[token.OP_ASSIGN] = "equal to"
-	reservedPhrases[token.OP_EQUAL] = "is equal to"
-	reservedPhrases[token.OP_NOT_EQUAL] = "does not equal"
-	reservedPhrases[token.OP_GREATER] = "is greater than"
-	reservedPhrases[token.OP_LESS] = "is less than"
-	reservedPhrases[token.OP_GREATER_OR_EQUAL] = "is greater than or equal to"
-	reservedPhrases[token.OP_LESS_OR_EQUAL] = "is less than or equal to"
-}
+// func init() {
+// 	reservedPhrases[token.KEYPHRASE_START] = "Please"
+// 	reservedPhrases[token.KEYPHRASE_END] = "Thanks"
+// 	reservedPhrases[token.KEYPHRASE_IF] = "check if"
+// 	reservedPhrases[token.KEYPHRASE_DECLARE] = "set"
+// 	reservedPhrases[token.KEYPHRASE_LOOP] = "loop while"
+// 	reservedPhrases[token.OP_ASSIGN] = "equal to"
+// 	reservedPhrases[token.OP_EQUAL] = "is equal to"
+// 	reservedPhrases[token.OP_NOT_EQUAL] = "does not equal"
+// 	reservedPhrases[token.OP_GREATER] = "is greater than"
+// 	reservedPhrases[token.OP_LESS] = "is less than"
+// 	reservedPhrases[token.OP_GREATER_OR_EQUAL] = "is greater than or equal to"
+// 	reservedPhrases[token.OP_LESS_OR_EQUAL] = "is less than or equal to"
+// }
 
 type Lexer struct {
 	input        string
@@ -30,6 +30,7 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
@@ -43,11 +44,28 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
-func (l *Lexer) NextToken() token.Token {
-	var token token.Token
-
-}
-
 func newToken(tokenType token.TokenType, literal string) token.Token {
 	return token.Token{Type: tokenType, Literal: literal}
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '(':
+		tok = newToken(token.LPAREN, "(")
+	case ')':
+		tok = newToken(token.RPAREN, ")")
+	case '{':
+		tok = newToken(token.LBRACE, "{")
+	case '}':
+		tok = newToken(token.RBRACE, "}")
+	case '#':
+		tok = newToken(token.COMMENT, "#")
+	case 0:
+		tok = newToken(token.EOF, "")
+	}
+
+	l.readChar()
+	return tok
 }
